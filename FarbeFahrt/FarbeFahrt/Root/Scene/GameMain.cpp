ce7f239345df7033d745fe3fe8dd6eca918ce9cc
aquaworks <aquaworks@live.jp>
2015-12-15 07:09:14 +0900

    リソース名が被ってたので回避 堀

--- FarbeFahrt/FarbeFahrt/Root/Scene/GameMain.cpp
+++ FarbeFahrt/FarbeFahrt/Root/Scene/GameMain.cpp
@@ -40,7 +40,7 @@ void GameMain::loadContents(Loader& loader)
 		loader.loadContent(resource.first, resource.second);
 	}*/
 
-	loader.loadContent("book", "Model/本/新本.pmx");
+	loader.loadContent("BaseBook", "Model/本/新本.pmx");
 	loader.loadContent("desk", "Model/机/つくえ.mqo");
 
 	loader.loadContent("TrueEnd", "Texture/end/true.png");
@@ -59,7 +59,7 @@ void GameMain::initialize()
 	/*m_world->addActor(ActorTag::Effect, std::make_shared<Field>(
 		*m_world, "book", Vector3(0.0f, -5.0f, 0.0f), 3.0f));*/
 	m_world->addActor(ActorTag::Effect, std::make_shared<AnimateActor>(
-		*m_world, "book", Vector3(0.0f, -5.0f, 0.0f), 3.0f));
+		*m_world, "BaseBook", Vector3(0.0f, -5.0f, 0.0f), 3.0f));
 	// 机
 	m_world->addActor(ActorTag::Effect, std::make_shared<StaticObject>(
 		*m_world, "desk", Vector3(-60.0f, -320.0f, 100.0f), (float)Math::ToRadian(-90.0), 0.8f));
@@ -68,7 +68,7 @@ void GameMain::initialize()
 	// m_stageManager.next(m_world.get());
 
 	// 次のステージへすぐ飛べるよう特別にフラグをtrueにする
-	m_stageManager.initialize("Resources/Script/Stage/index.csv", "PlainA");
+	m_stageManager.initialize("Resources/Script/Stage/index.csv", "Lowles");
 	StoryManager::set(BitFlag::GOAL);
 
 	EndManager::Clear();
@@ -76,7 +76,7 @@ void GameMain::initialize()
 	AnimateActor::AnimationInfo info;
 	info.animationNumber = 0;
 	info.animationTime = 180.0f;
-	m_world->findActor("book")->sendMessage("Animate", &info);
+	m_world->findActor("BaseBook")->sendMessage("Animate", &info);
 }
 
 void GameMain::update()
@@ -133,7 +133,7 @@ void GameMain::post()
 				info.animationNumber = 0;
 				info.animationTime = 180.0f;
 				info.isReversed = true;
-				m_world->findActor("book")->sendMessage("Animate", &info);
+				m_world->findActor("BaseBook")->sendMessage("Animate", &info);
 			}
 			return;
 		}
