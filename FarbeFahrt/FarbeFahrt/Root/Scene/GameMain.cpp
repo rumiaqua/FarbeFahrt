71c642efffe9a62e1a0d8c159e2d724ff71ca33c
naritan <naritancorp@docomo.ne.jp>
2016-01-26 02:09:25 +0900

    とりあえずリソース変わってなかったから変えたよ なりたん

--- FarbeFahrt/FarbeFahrt/Root/Scene/GameMain.cpp
+++ FarbeFahrt/FarbeFahrt/Root/Scene/GameMain.cpp
@@ -63,7 +63,7 @@ void GameMain::initialize()
 	// m_stageManager.next(m_world.get());
 
 	// 次のステージへすぐ飛べるよう特別にフラグをtrueにする
-	m_stageManager.initialize("Resources/Script/Stage/index.csv", "Heiles");
+	m_stageManager.initialize("Resources/Script/Stage/index.csv", "PlainB");
 	StoryManager::set(BitFlag::GOAL);
 
 	EndManager::Clear();
